6f09edbcd96adb4668ac98d9186d79a9a933ef0e
Suvit <suvit@DESKTOP-GHQVJGR.localdomain>
2020-03-02 08:54:34 +0530

    added routes updated

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,19 @@ from werkzeug.utils import secure_filename
 @app.route('/index') 
 def index():
         return "Hello, World!"
+@app.route('/lodge_complaint',methods=['GET', 'POST'])
+def file_fir():
+    if request.method == 'GET':
+        return render_template('file_complaint.html')
+    if request.method == 'POST':
+        fid = request.form["f_id"] 
+        c_name = request.form["c_name"] 
+        c_date= request.form["c_date"] 
+        c_time = request.form["c_time"] 
+        c_place = request.form["c_place"] 
+        c_cat = request.form["cat"] 
+        c_text = request.form["c_text"] 
+        return ""+fid+" "+c_name+" "+c_date+" "+c_time+" "+c_place+" "+c_cat+" "+c_text 
 
 @app.route('/upload')
 def load_file():
